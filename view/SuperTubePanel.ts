8db4984afb0469f13cadec9ae7a67c3ee300c87f
shiny <hnhung123@gmail.com>
2019-07-26 20:51:13 +0800

    Set UI for each game accordingly

--- view/SuperTubePanel.ts
+++ view/SuperTubePanel.ts
@@ -1,6 +1,6 @@
 class SuperTubePanel extends JTPanel<supertube.SuperTubeView>{
     private plus: SuperTubeNetPlus;
-    public static cutGameEnter:number=0
+    public static cutEnterGame:number=0
     private areaBetData:number[]=[];
     private _ares:number[]=[];
     constructor(){
@@ -20,12 +20,23 @@ class SuperTubePanel extends JTPanel<supertube.SuperTubeView>{
     }
     private initUI():void{
        this.view.closeButton.onClick(this, this.closeGameEndView);
+       this.view.c1.selectedIndex = SuperTubePanel.cutEnterGame;
+       this.view.betAreacmpt.c1.selectedIndex = SuperTubePanel.cutEnterGame;
+       this.view.playerlistcmpt.c1.selectedIndex = SuperTubePanel.cutEnterGame;
     };
     public showBetAreaData(aemoneys:number[],areas:number[]=[]):void{
         this.areaBetData=aemoneys;
         this._ares=areas;
         console.log("区域11：", aemoneys, areas);
-        let _arealist=this.view.betAreacmpt["arealist_"+SuperTubePanel.cutGameEnter] as fairygui.GList
+        let _arealist=this.view.betAreacmpt["arealist_"+SuperTubePanel.cutEnterGame] as fairygui.GList;
+        for(var i=0;i<=aemoneys.length;i++){
+            if(i<=_arealist.numItems-1){
+                let areaitem=_arealist.getChildAt(i).asLabel;
+                areaitem.title=RandomUtil.formatGold(aemoneys[i]-1)
+            }
+        }
+        (this.view.betAreacmpt["win_label"] as fairygui.GLabel).title="0";
+
     }
     public closeGameEndView():void{
         this.plus.remove();
